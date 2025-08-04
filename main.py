78d27fe84c6866e1d6d8fbc9ab945e353cb3bb68
Grant <gstoltman@gmail.com>
2025-08-04 10:30:13 -0700

    initial creation

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
+
+def main():
+    load_dotenv()
+
+    args = sys.argv[1:]
+
+    if not args:
+        print("ERROR")
+        sys.exit(1)
+
+    user_prompt = " ".join(args)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents=messages
+    )
+
+    if "--verbose" in args:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print("Response:")
+
+    print(response.text)
+
+
+
+if __name__ == "__main__":
+    main()
