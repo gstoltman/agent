3d6ab933d4a2d9c568ed601f7e98c9d385d30f67
Grant <gstoltman@gmail.com>
2025-08-04 17:16:25 -0700

    not working, fix tomorrow

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,21 +1,25 @@
 import os
 
 def get_files_info(working_directory, directory="."):
-    full_path = os.path.join(working_directory, directory)
-    abs_path = os.path.abspath(directory)
+    abs_working_directory = os.path.abspath(working_directory)
+    full_path = os.path.abspath(os.path.join(working_directory, directory))
 
-    if not abs_path.startswith(working_directory):
+    print(f"abs_working_directory is {abs_working_directory}")
+    print(f"abs_full_path is {os.path.abspath(full_path)}")
+    
+    if os.path.commonpath([abs_working_directory, full_path]) != abs_working_directory:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    if not os.path.isdir(directory):
+    if not os.path.isdir(full_path):
         return f'Error: "{directory}" is not a directory'
 
-    contents_list = os.listdir(directory)
+    contents_list = os.listdir(full_path)
 
     result_text = ''
 
     for content in contents_list:
-        file_size = os.path.isfile(content)
-        is_dir = os.path.isdir(content)
-        result_text.join(content, file_size, is_dir, '\n')
+        content_path = os.path.join(full_path, content)
+        file_size = os.path.getsize(content_path)
+        is_dir = os.path.isdir(content_path)
+        result_text.join(f"{content} | File Size: {file_size} | Directory: {is_dir}")
 
     return result_text
